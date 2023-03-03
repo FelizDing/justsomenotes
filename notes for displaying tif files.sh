3c79ea1896c01ec9c00e77a2843e79fd022e4f48
FelizDing <78589728+FelizDing@users.noreply.github.com>
2023-03-03 11:41:19 -0800

    Update notes for displaying tif files.sh

--- notes for displaying tif files.sh
+++ notes for displaying tif files.sh
@@ -1,5 +1,5 @@
 #locate to the folder where you store all the tifs
-$ mkdir movies
-$ for f in *.tif; do outname=${f%.*}'.mp4'; echo $f '==>' 'movies/'$outname; done
-$ for f in *.tif; do outname=${f%.*}'.mp4'; convert_tif_to_mp4.py $f 'movies/'$outname -slow_down_factor 0.01; done
-$ make_montage.sh ./movies > look_at_me.html
+  mkdir movies
+  for f in *.tif; do outname=${f%.*}'.mp4'; echo $f '==>' 'movies/'$outname; done
+  for f in *.tif; do outname=${f%.*}'.mp4'; convert_tif_to_mp4.py $f 'movies/'$outname -slow_down_factor 0.01; done
+  make_montage.sh ./movies > look_at_me.html
