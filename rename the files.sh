360fb39f9888804c8a6dcd5faba3c2282958c9c7
FelizDing <78589728+FelizDing@users.noreply.github.com>
2023-03-03 11:40:54 -0800

    Update rename the files.sh

--- rename the files.sh
+++ rename the files.sh
@@ -1,4 +1,3 @@
-# shellcheck shell=bash
 #rename the files，I want to swap "UD916 lmn-1 RNAi" part in the strings to "hyp_lmn-1(R64P)" while other strings remain.
 #cd to the folder contains files
-$ for file in *; do mv "$file" "${file/UD916 lmn-1 RNAi/hyp_lmn-1(R64P)}"; done
+  for file in *; do mv "$file" "${file/UD916 lmn-1 RNAi/hyp_lmn-1(R64P)}"; done
